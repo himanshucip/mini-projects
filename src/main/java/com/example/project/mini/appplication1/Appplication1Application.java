ae2b853cd92c9d16e4dd5dcbf30f8df1ea1971f7
himanshu <kashking474@gmail.com>
2023-07-30 11:43:16 +0530

    first-commit

---
+++ src/main/java/com/example/project/mini/appplication1/Appplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.project.mini.appplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appplication1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/mini/appplication1/Appplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.mini.appplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
